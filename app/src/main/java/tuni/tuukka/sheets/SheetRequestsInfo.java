f980c07496da46d01752d5110e5adf540994038d
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-13 10:43:39 +0200

    SheetRequestInfo: Add

---
+++ app/src/main/java/tuni/tuukka/sheets/SheetRequestsInfo.java
@@ -0,0 +1,11 @@
+package tuni.tuukka.sheets;
+
+public class SheetRequestsInfo {
+    String sheetID;
+    String range;
+
+    public SheetRequestsInfo(String sheetID, String range) {
+        this.sheetID = sheetID;
+        this.range = range;
+    }
+}
