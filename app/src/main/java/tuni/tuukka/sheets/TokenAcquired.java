70c0dfaf25ccb244d03672bbe1c4f786c1eabd7c
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-15 09:45:58 +0200

    TokenAcquired: Saves token to sharedPreferences
    - Add Context parameter to constructor in order to access
    sharedPreferences.
    
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/sheets/TokenAcquired.java
+++ app/src/main/java/tuni/tuukka/sheets/TokenAcquired.java
@@ -5,6 +5,7 @@ import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,27 +13,26 @@ import java.io.IOException;
 import java.util.List;
 
 public class TokenAcquired implements AccountManagerCallback<Bundle> {
+    Context context;
+    public TokenAcquired(Context context) {
+        this.context = context;
+    }
+
     @Override
     public void run(AccountManagerFuture<Bundle> accountManagerFuture) {
-        Log.d("tuksu", "run: ACCOUNTMANAGERCALLBACK");
-
         try {
-            Log.d("tuksu", "run: " + accountManagerFuture.isDone() + accountManagerFuture.isCancelled());
-
             Bundle bundle = accountManagerFuture.getResult();
             String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
-            Token.setToken(token);
-            new SheetReader().execute();
+            Token.saveToken(token, context);
 
-            Log.d("tuksu", "run: GOT KEY" + token);
         } catch (AuthenticatorException e) {
             e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
+
         } catch (OperationCanceledException e) {
             e.printStackTrace();
         }
-
-
     }
 }
