c1b70f46ed9744c58b4f5bced21da41844ab31e7
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-15 09:38:34 +0200

    Token: Add saveToken() and loadToken()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/sheets/Token.java
+++ app/src/main/java/tuni/tuukka/sheets/Token.java
@@ -1,8 +1,16 @@
 package tuni.tuukka.sheets;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import java.util.Optional;
 
-public class Token {
+public class Token extends Application {
+    private static final String TOKEN_NAME= "token";
+    private static final String PREFERENCES_NAME = "tuni.tuukka.time-tracker";
     private static Optional<String> token;
     private static boolean tokenSet = false;
 
@@ -12,5 +20,17 @@ public class Token {
 
     static void setToken(String newToken) {
         token = Optional.of(newToken);
+    }
+
+    static void saveToken(String token, Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(TOKEN_NAME, token);
+        editor.apply();
+    }
+
+    static String loadToken(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        return preferences.getString(TOKEN_NAME, null);
     }
 }
