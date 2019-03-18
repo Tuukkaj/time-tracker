11d28e3212cb49e098b960ddb1d1d42b5b01938d
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-18 21:41:37 +0200

    GoogleService: Add
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

---
+++ app/src/main/java/tuni/tuukka/google/GoogleService.java
@@ -0,0 +1,21 @@
+package tuni.tuukka.google;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.sheets.v4.Sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class GoogleService {
+    private static final String APP_NAME = "time-tracker";
+
+    protected static Drive createDriveService(String token) throws IOException, GeneralSecurityException {
+        Credential credential = new GoogleCredential().setAccessToken(token);
+        return new Drive.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
+                credential).setApplicationName(APP_NAME).build();
+    }
+}
