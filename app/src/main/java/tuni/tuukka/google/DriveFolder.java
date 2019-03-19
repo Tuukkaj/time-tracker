f23b8e0af1db9d38414fe1d2fa7bba39541cb76b
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-19 17:38:21 +0200

    DriveFolder: Add createNewFolder()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/DriveFolder.java
+++ app/src/main/java/tuni/tuukka/google/DriveFolder.java
@@ -22,13 +22,14 @@ public class DriveFolder {
                 try {
                     Drive drive = DriveService.createDriveService(Token.getToken().get());
                     List<File> files = drive.files().list().setQ("name = '"+folderName+"'").execute().getFiles();
+
                     try {
                         JSONArray array = new JSONArray(files.toString());
                         JSONObject object = array.getJSONObject(0);
                         String name = object.getString("name");
                         String folderId = object.getString("id");
-                        System.out.println("Name: " + name + " Id" + folderId);
                         if(name.equalsIgnoreCase(folderName)) {
+                            System.out.println("Name: " + name + " Id" + folderId);
                             checkReady.doAfter(folderId);
                         } else {
                             checkReady.onNoFolderFound();
@@ -45,6 +46,27 @@ public class DriveFolder {
                 } catch (Exception e) {
                     e.printStackTrace();
                     checkReady.onFail();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    public static void createNewFolder(String name) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Drive drive = DriveService.createDriveService(Token.getToken().get());
+                    File folder = new File();
+                    folder.setName(name);
+                    folder.setMimeType("application/vnd.google-apps.folder");
+                    File createdFolder = drive.files().create(folder).setFields("id").execute();
+                    System.out.println(createdFolder.getId());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
                 }
                 return null;
             }
