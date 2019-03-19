537df687957b07ba744cabf4d31014a03a213686
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-19 08:40:32 +0200

    DriveFolder: Rework how AsyncTask is called
    - Add checkFolders() which handles everything related to AsyncTask.
    - Add interface FolderCheckReady which is called if something goes
    wrong.
    
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/DriveFolder.java
+++ app/src/main/java/tuni/tuukka/google/DriveFolder.java
@@ -4,29 +4,38 @@ import android.os.AsyncTask;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
+
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
-public class DriveFolder extends AsyncTask<Void, Void, Void> {
-    private void getDriveFolders() {
-        try {
-            Drive drive = DriveService.createDriveService(Token.getToken().get());
-            FileList list = drive.files().list().execute();
-            List<File> files = list.getFiles();
-            System.out.println(files);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
+public class DriveFolder {
+    public static void checkFolders(FolderCheckReady checkReady) {
+        AsyncTask task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Drive drive = DriveService.createDriveService(Token.getToken().get());
+                    List<File> files = drive.files().list().setQ("name = 'Arkisto'").execute().getFiles();
+                    System.out.println(files);
+                    //PLACEHOLDER
+                    checkReady.doAfter(true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    checkReady.onFail();
+                }
+                return null;
+            }
+        }.execute();
     }
 
-    @Override
-    public Void doInBackground(Void... voids) {
-        getDriveFolders();
-        return null;
+    public interface FolderCheckReady {
+        void doAfter(boolean result);
+        void onFail();
     }
 }
