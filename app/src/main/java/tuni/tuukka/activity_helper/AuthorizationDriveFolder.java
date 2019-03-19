a8fb8394bff542a90817beffb1bac54aacf50760
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-19 16:46:41 +0200

    AuthorizationDriveFolder: Add interfaceGetFiles()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activity_helper/AuthorizationDriveFolder.java
+++ app/src/main/java/tuni/tuukka/activity_helper/AuthorizationDriveFolder.java
@@ -1,4 +1,32 @@
 package tuni.tuukka.activity_helper;
 
+import android.app.Activity;
+import android.widget.Toast;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+
+import tuni.tuukka.google.AccountAuthorization;
+import tuni.tuukka.google.DriveFolder;
+
 public class AuthorizationDriveFolder {
+    public static DriveFolder.FolderCheckReady interfaceGetFiles(Activity activity, GoogleAccountCredential credential) {
+        return new DriveFolder.FolderCheckReady() {
+            @Override
+            public void doAfter(boolean result, String folderId) {
+                System.out.println(result);
+                System.out.println(folderId);
+            }
+
+            @Override
+            public void onFail() {
+                activity.runOnUiThread(() -> Toast.makeText(activity, "Please try again", Toast.LENGTH_SHORT).show());
+                AccountAuthorization.authorize(activity, credential);
+            }
+
+            @Override
+            public void onNoFolderFound() {
+
+            }
+        };
+    }
 }
