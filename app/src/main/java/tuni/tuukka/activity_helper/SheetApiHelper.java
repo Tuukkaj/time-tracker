5a83119cf1000cf50736fec693fc49947b3201f5
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-20 16:34:42 +0200

    SheetApiHelper: Add interfaceReadSheet()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activity_helper/SheetApiHelper.java
+++ app/src/main/java/tuni/tuukka/activity_helper/SheetApiHelper.java
@@ -1,4 +1,26 @@
 package tuni.tuukka.activity_helper;
 
+import android.app.Activity;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+
+import java.util.List;
+
+import tuni.tuukka.google.AccountAuthorization;
+import tuni.tuukka.google.SheetApi;
+
 public class SheetApiHelper {
+    public SheetApi.ReadSheetInterface interfaceReadSheet(Activity activity, GoogleAccountCredential credential) {
+        return new SheetApi.ReadSheetInterface() {
+            @Override
+            public void onFail() {
+                AccountAuthorization.authorize(activity, credential);
+            }
+
+            @Override
+            public void onSuccess(List<List<Object>> values) {
+                System.out.println(values);
+            }
+        };
+    }
 }
