3d9cd36c8e49f1e460797cc0ed35ee55c97a7c40
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-20 16:31:23 +0200

    SheetApi: Reformat readFromSheet
    - Rename SheetApi.AuthenticationFailed to ReadSheetInterface
    - Add method to SheetApi.ReadSheetInterface
    
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/SheetApi.java
+++ app/src/main/java/tuni/tuukka/google/SheetApi.java
@@ -10,48 +10,39 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
-public class SheetApi extends AsyncTask<SheetRequestsInfo, Void, Void> {
-    AuthenticationFailed call;
-
-    public SheetApi(AuthenticationFailed call) {
-        this.call = call;
-    }
-    public List<List<Object>> readFromSheet(String sheetID, String range){
-        try{
-            if(Token.getToken().isPresent()) {
-                Sheets service = SheetsService.createSheetService(Token.getToken().get());
-                ValueRange response = service.spreadsheets().values()
-                        .get(sheetID, range)
-                        .execute();
-                return response.getValues();
-            } else {
+public class SheetApi {
+    public static void readSheet(SheetRequestsInfo info, ReadSheetInterface sheetInterface) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try{
+                    if(Token.getToken().isPresent()) {
+                        Sheets service = SheetsService.createSheetService(Token.getToken().get());
+                        ValueRange response = service.spreadsheets().values()
+                                .get(info.sheetID, info.range)
+                                .execute();
+                        System.out.println(response.getValues());
+                    } else {
+                        return null;
+                    }
+                } catch (GoogleJsonResponseException e){
+                    e.printStackTrace();
+                    System.out.println("EXCPETION GoogleJsonResponseException");
+                    sheetInterface.onFail();
+                }  catch (IOException e) {
+                    sheetInterface.onFail();
+                    e.printStackTrace();
+                } catch (GeneralSecurityException e) {
+                    sheetInterface.onFail();
+                    e.printStackTrace();
+                }
                 return null;
             }
-        } catch (GoogleJsonResponseException e){
-            System.out.println("EXCPETION GoogleJsonResponseException");
-            call.call();
-        }  catch (IOException e) {
-            e.printStackTrace();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
-        return null;
+        }.execute();
     }
 
-    @Override
-    protected Void doInBackground(SheetRequestsInfo... info) {
-        SheetRequestsInfo sheetInfo = info[0];
-                List<List<Object>> values = readFromSheet(sheetInfo.sheetID, sheetInfo.range);
-
-        if (values == null || values.isEmpty()) {
-            System.out.println("No data found.");
-        } else {
-            System.out.println(values);
-        }
-        return null;
-    }
-
-    public interface AuthenticationFailed {
-        public void call();
+    public interface ReadSheetInterface {
+        public void onFail();
+        public void onSuccess(List<List<Object>> values);
     }
 }
