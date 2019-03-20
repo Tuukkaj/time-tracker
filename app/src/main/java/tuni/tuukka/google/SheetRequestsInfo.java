ad86859c77901788e4eef02109697d861b6f298f
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-20 16:40:15 +0200

    SheetRequestInfo: Add new variable and constructor for List<String>
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/SheetRequestsInfo.java
+++ app/src/main/java/tuni/tuukka/google/SheetRequestsInfo.java
@@ -1,11 +1,20 @@
 package tuni.tuukka.google;
 
+import java.util.List;
+
 public class SheetRequestsInfo {
     String sheetID;
     String range;
+    List<String> ranges;
 
     public SheetRequestsInfo(String sheetID, String range) {
         this.sheetID = sheetID;
         this.range = range;
     }
+
+    public SheetRequestsInfo(String sheetID, List<String> ranges) {
+        this.sheetID = sheetID;
+        this.ranges = ranges;
+    }
+
 }
