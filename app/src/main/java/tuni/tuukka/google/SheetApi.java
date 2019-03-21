0486530d3d60d03b41f793dbd06a5b5b06ceeb34
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 22:13:49 +0200

    SheetApi: Add appendData()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/SheetApi.java
+++ app/src/main/java/tuni/tuukka/google/SheetApi.java
@@ -4,11 +4,13 @@ import android.os.AsyncTask;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SheetApi {
@@ -67,6 +69,37 @@ public class SheetApi {
                     e.printStackTrace();
                 } catch (GeneralSecurityException e) {
                     readInterface.onFail();
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    public static void appendSheet(AppendData data) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                ValueRange values = new ValueRange();
+                List<Object> objects = new ArrayList<>();
+                objects.add(data.start);
+                objects.add(data.end);
+                objects.add(data.category);
+                objects.add(data.comment);
+                List<List<Object>> list = new ArrayList<>();
+                list.add(objects);
+                values.setValues(list);
+
+                try {
+                    Sheets sheetService = SheetsService.createSheetService(Token.getToken().get());
+                    Sheets.Spreadsheets.Values.Append request = sheetService.spreadsheets().values().append(data.info.sheetID, data.info.range, values);
+                    request.setInsertDataOption("INSERT_ROWS");
+                    request.setValueInputOption("RAW");
+                    AppendValuesResponse response = request.execute();
+                    System.out.println(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (GeneralSecurityException e) {
                     e.printStackTrace();
                 }
                 return null;
