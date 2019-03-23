7815308db18060d507f7bbb7006104c84b99418a
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-23 23:17:22 +0200

    AccountAuthorization: Add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/AccountAuthorization.java
+++ app/src/main/java/tuni/tuukka/google/AccountAuthorization.java
@@ -8,8 +8,19 @@ import android.util.Log;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.services.drive.DriveScopes;
-
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @since       1.8
+ *
+ * Asks users permissions for using scope DriveScopes.DRIVE in Google APIs.
+ */
 public class AccountAuthorization {
+    /**
+     * Authorizes given credential to use scope DriveScopes.DRIVE. Does this in given activitys
+     * UI thread
+     * @param activity Activity to ask permissions
+     * @param credential Credentials to authorize
+     */
     public static void authorize(Activity activity, GoogleAccountCredential credential) {
         activity.runOnUiThread(() -> {
             AccountManager am = AccountManager.get(activity);
@@ -17,7 +28,7 @@ public class AccountAuthorization {
             am.getAuthToken(credential.getSelectedAccount(), "oauth2:" + DriveScopes.DRIVE,
                     options, activity, new TokenAcquired(activity),
                     new Handler(message -> {
-                        Log.d("tuksu", "ErrorHandler: " + message);
+                        System.out.println(message);
                         return true;
                     }));
         });
