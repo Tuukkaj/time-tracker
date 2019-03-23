d5f18762a209b86fb46dc0a9a6a2785b80b84a4b
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-23 23:13:42 +0200

    Authorization: Add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/Authorization.java
+++ app/src/main/java/tuni/tuukka/activities/Authorization.java
@@ -40,16 +40,43 @@ import tuni.tuukka.google.SheetApi;
 import tuni.tuukka.google.SheetRequestsInfo;
 import tuni.tuukka.google.Token;
 
+/**
+ * Handles everything related to authorization of Google Account.
+ */
 public class Authorization extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+
+    /**
+     * Request code for account picker.
+     */
     static final int REQUEST_ACCOUNT_PICKER = 1000;
+
+    /**
+     * Request code for authorization.
+     */
     static final int REQUEST_AUTHORIZATION = 1001;
+
+    /**
+     * Request code for play services.
+     */
     static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+
+    /**
+     * Request code for getting accounts.
+     */
     static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
 
     private static final String PREF_ACCOUNT_NAME = "accountName";
 
+    /**
+     * Users credentials. Used for authorization of application and getting OAuth2 token.
+     */
     GoogleAccountCredential credential;
 
+    /**
+     * Sets up users GoogleAccountCredential
+     *
+     * @param savedInstanceState Saved instance. Not in use.
+     */
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +85,12 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         credential = GoogleAccountCredential.usingOAuth2(getApplicationContext(), scopes).setBackOff(new ExponentialBackOff());
     }
 
+    /**
+     * Calls SheetApi or DriveApi based on what button was clicked.
+     * !Placeholder, will be changed in later releases!
+     *
+     * @param v view that was clicked.
+     */
     public void buttonClick(View v) {
         Optional<String> token = Token.loadToken(this);
         switch (v.getId()) {
@@ -94,6 +127,15 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
             }
         }
     }
+
+    /**
+     * Checks if:
+     *  - Google Play Services are available.
+     *  - GoogleAccountCredentials are okay.
+     *  - Device is online
+     *
+     * Sets buttons visible and authorizes account when all checks are ready.
+     */
     public void login() {
         if(!isGoogleServicesAvailable()) {
             acquirePlayServices();
@@ -119,6 +161,10 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         ((Button) findViewById(R.id.getFolder)).setVisibility(View.VISIBLE);
     }
 
+    /**
+     * Checks if user has selected account to use before. If has saved account loads it, if not
+     * starts startActivityForResult()
+     */
     @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
     private void chooseAccount() {
         if(EasyPermissions.hasPermissions(this, Manifest.permission.GET_ACCOUNTS)) {
@@ -137,6 +183,9 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         }
     }
 
+    /**
+     * Checks if Google Play Services are available.
+     */
     private void acquirePlayServices() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
 
@@ -147,20 +196,30 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         }
     }
 
+    /**
+     * Shows error dialog if Play services are unavailable.
+     * @param connectionStatusCode Status code to show in dialog.
+     */
     private void showPlayServiceErrorDialog(final int connectionStatusCode) {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         Dialog dialog = apiAvailability.getErrorDialog(this, connectionStatusCode, REQUEST_GOOGLE_PLAY_SERVICES);
         dialog.show();
     }
 
-    //Tests
-
+    /**
+     * Checks if device is online.
+     * @return True if device is online. False if not.
+     */
     private boolean isDeviceOnline() {
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = manager.getActiveNetworkInfo();
         return (info != null && info.isConnected());
     }
 
+    /**
+     * Checks if Play services are available.
+     * @return True if Play services are available. False if not.
+     */
     private boolean isGoogleServicesAvailable() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
 
@@ -169,14 +228,22 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
 
-    // Overridden methods
+    /**
+     * Receives calls when activity for results are finished. Has switch case for Authorizations
+     * request codes. If code is
+     * - REQUEST_GOOGLE_PLAY_SERVICES: Creates toast for user
+     * - REQUEST_ACCOUNT_PICKER: Saves account name key in preferences. Proceeds login if OK.
+     * - REQUEST_AUTHORIZATION: Proceeds login if OK.
+     * @param requestCode Request code for startActivityForResult
+     * @param resultCode Result code to check if result is OK
+     * @param data possible data from activity.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
             case REQUEST_GOOGLE_PLAY_SERVICES:
-                Log.d("tuksu", "OnActivityResult - REQUEST_GOOGLE_PLAY_SERVICES");
                 if(requestCode != RESULT_OK) {
                     Toast.makeText(this, "Please install Google Play services from App store. Then relaunch app.", Toast.LENGTH_SHORT).show();
                 } else {
@@ -185,10 +252,9 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
                 break;
 
             case REQUEST_ACCOUNT_PICKER:
-                Log.d("tuksu", "OnActivityResult - REQUEST_ACCOUNT_PICKER");
                 if(resultCode == RESULT_OK && data != null && data.getExtras() != null) {
                     String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    Log.d("tuksu", "OnActivityResult - REQUEST_ACCOUNT_PICKER p2 " + accountName);
+
                     if(accountName != null) {
                         SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = settings.edit();
@@ -196,7 +262,7 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
                         editor.putString(PREF_ACCOUNT_NAME, accountName);
                         editor.apply();
                         credential.setSelectedAccountName(accountName);
-                        Log.d("tuksu", "OnActivityResult - REQUEST_ACCOUNT_PICKER p2 " + credential.getSelectedAccountName());
+
                         login();
                     }
                 }
@@ -210,6 +276,12 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
         }
     }
 
+    /**
+     * Calls EasyPermissions if permissions were granted
+     * @param requestCode Request code given
+     * @param permissions Permissions given
+     * @param grantResults Granted results
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -219,11 +291,21 @@ public class Authorization extends AppCompatActivity implements EasyPermissions.
                 requestCode, permissions, grantResults, this);
     }
 
+    /**
+     * Does nothing when called
+     * @param requestCode Request code given
+     * @param perms Permissions given
+     */
     @Override
     public void onPermissionsGranted(int requestCode, List<String> perms) {
         //Do nothing
     }
 
+    /**
+     * Does nothing when called
+     * @param requestCode Request code given
+     * @param perms Permissions given
+     */
     @Override
     public void onPermissionsDenied(int requestCode, List<String> perms) {
         // Do nothing
