5bbff2f129a17463f04e14ce5166b4dd1cfe3626
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:35:09 +0200

    Data: Change to abstract and add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/Data.java
+++ app/src/main/java/tuni/tuukka/google/Data.java
@@ -1,8 +1,23 @@
 package tuni.tuukka.google;
 
-public class Data {
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @version     20190323
+ * @since       1.8
+ *
+ * Data class that holds SheetRequestInfo of Sheet Id and range. Used as base class for passing
+ * data to to SheetApi.AppendSheet().
+ */
+public abstract class Data {
+    /**
+     * Holds target sheetId and range
+     */
     public SheetRequestsInfo info;
 
+    /**
+     * Sets parameter to class variable.
+     * @param info Target sheetId and range.
+     */
     public Data(SheetRequestsInfo info) {
         this.info = info;
     }
