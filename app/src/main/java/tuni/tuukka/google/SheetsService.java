b68643881a3bfac90983123dee39571749386922
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:50:42 +0200

    SheetsService: Add Javadoc.
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/SheetsService.java
+++ app/src/main/java/tuni/tuukka/google/SheetsService.java
@@ -9,9 +9,27 @@ import com.google.api.services.sheets.v4.Sheets;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @version     20190324
+ * @since       1.8
+ *
+ * Used to create Sheets for SheetApi class
+ */
 public class SheetsService {
+    /**
+     * Name of app.
+     */
     private static final String APP_NAME = "time-tracker";
 
+    /**
+     * Creates Sheets service from given token. Used by SheetApi class to get Sheets class.
+     *
+     * @param token Token to initialize creation of Sheets
+     * @return Sheets service created with given Token
+     * @throws IOException Thrown if communication with Google fails.
+     * @throws GeneralSecurityException Thrown if there is security exception occurs.
+     */
     protected static Sheets createSheetService(String token) throws IOException, GeneralSecurityException {
         Credential credential = new GoogleCredential().setAccessToken(token);
         return new Sheets.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
