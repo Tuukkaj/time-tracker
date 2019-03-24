754fdbee47f3c82f6ca6d1b4bb60209d4d784e44
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:48:51 +0200

    DriveService: Add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/DriveService.java
+++ app/src/main/java/tuni/tuukka/google/DriveService.java
@@ -10,9 +10,28 @@ import com.google.api.services.sheets.v4.Sheets;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @version     20190324
+ * @since       1.8
+ *
+ * Used to create Drive service for DriveApi class
+ */
 public class DriveService {
+
+    /**
+     * Name of the app
+     */
     private static final String APP_NAME = "time-tracker";
 
+    /**
+     * Creates drive service from given token. Used by DriveApi class to get Drive class.
+     *
+     * @param token Token to initialize creation of Drive
+     * @return Drive service created with given Token
+     * @throws IOException Thrown if communication with Google fails.
+     * @throws GeneralSecurityException Thrown if there is security exception occours.
+     */
     protected static Drive createDriveService(String token) throws IOException, GeneralSecurityException {
         Credential credential = new GoogleCredential().setAccessToken(token);
         return new Drive.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance(),
