131a28087ba83a6c7f7ebea3f57aba720b052d9b
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:43:13 +0200

    DoAfter: Add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/DoAfter.java
+++ app/src/main/java/tuni/tuukka/google/DoAfter.java
@@ -1,5 +1,14 @@
 package tuni.tuukka.google;
 
+/**
+ * Interface for calling DriveApi and SheetApi. Used to pass commands for what to do in case of
+ * success on Drive/Sheet operation or in case of failure.
+ * @param <T> What onSuccess() accepts as a result.
+ */
 public interface DoAfter<T> extends OnFail{
+    /**
+     * Called when operation was successful.
+     * @param value Result value from operation.
+     */
     void onSuccess(T value);
 }
