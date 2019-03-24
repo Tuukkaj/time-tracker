0b2d529a0eff71426516ffecdc4f6b8c185b6fd1
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:44:57 +0200

    OnFail: Add Javadoc.
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/OnFail.java
+++ app/src/main/java/tuni/tuukka/google/OnFail.java
@@ -1,5 +1,15 @@
 package tuni.tuukka.google;
 
+/**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @version     20190324
+ * @since       1.8
+ *
+ * Interface for communicating what to do if Drive/Sheet operation fails.
+ */
 public interface OnFail {
+    /**
+     * Commands for recovery from failure to fetch/post data to Drive/Sheets.
+     */
     void onFail();
 }
