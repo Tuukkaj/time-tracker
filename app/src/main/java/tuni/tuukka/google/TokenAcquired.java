c060ce76a0949a041ed444fe2961a92c33b1409f
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 13:02:44 +0200

    TokenAcquired: Add Javadoc.
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/TokenAcquired.java
+++ app/src/main/java/tuni/tuukka/google/TokenAcquired.java
@@ -10,12 +10,27 @@ import android.os.Bundle;
 
 import java.io.IOException;
 
+/**
+ * Used to set token when user loads AccountManager gets token from Google.
+ */
 public class TokenAcquired implements AccountManagerCallback<Bundle> {
+    /**
+     * Context used for saving token.
+     */
     Context context;
+
+    /**
+     * Sets parameter to class variable.
+     * @param context Context used for saving token.
+     */
     public TokenAcquired(Context context) {
         this.context = context;
     }
 
+    /**
+     * Saves token when acquired from AccountManager.
+     * @param accountManagerFuture Used for getting token.
+     */
     @Override
     public void run(AccountManagerFuture<Bundle> accountManagerFuture) {
         try {
