99204b31a38a0d6d21014f3ff349d39f11b25dc9
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:14:40 +0200

    SheetRequestInfo: Add method getRanges()
    - SheetRequestInfo: Modify TIME_RANGE
    
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/SheetRequestsInfo.java
+++ app/src/main/java/tuni/tuukka/google/SheetRequestsInfo.java
@@ -1,13 +1,21 @@
 package tuni.tuukka.google;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SheetRequestsInfo {
-    public static final String TIME_RANGE = "Sheet1!A:A";
+    public static final String TIME_RANGE = "Sheet1!A:D";
     public static final String CATEGORY_RANGE = "Sheet2!A:A";
     String sheetID;
     String range;
     List<String> ranges;
+
+    public static List<String> getRanges() {
+        List<String> list = new ArrayList<>();
+        list.add(TIME_RANGE);
+        list.add(CATEGORY_RANGE);
+        return list;
+    }
 
     public SheetRequestsInfo(String sheetID, String range) {
         this.sheetID = sheetID;
