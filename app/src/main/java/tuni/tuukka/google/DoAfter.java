90fcf51c597304249d66c71da8387fcf218c2cc8
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-24 12:44:46 +0200

    DoAfter: Update Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/google/DoAfter.java
+++ app/src/main/java/tuni/tuukka/google/DoAfter.java
@@ -1,6 +1,10 @@
 package tuni.tuukka.google;
 
 /**
+ * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
+ * @version     20190324
+ * @since       1.8
+ *
  * Interface for calling DriveApi and SheetApi. Used to pass commands for what to do in case of
  * success on Drive/Sheet operation or in case of failure.
  * @param <T> What onSuccess() accepts as a result.
