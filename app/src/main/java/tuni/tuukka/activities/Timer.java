a7e902396c5b09f9a8446e4cfc0723f814d74d22
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-01 18:31:33 +0300

    Timer: Add onClick()
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/Timer.java
+++ app/src/main/java/tuni/tuukka/activities/Timer.java
@@ -3,6 +3,8 @@ package tuni.tuukka.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import tuni.tuukka.R;
 
@@ -11,5 +13,17 @@ public class Timer extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.start_button) {
+            Button start = (Button) findViewById(R.id.start_button);
+            Button end = (Button) findViewById(R.id.end_button);
+
+            start.setEnabled(false);
+            end.setEnabled(true);
+        } else if (v.getId() == R.id.end_button){
+            Button end = (Button) findViewById(R.id.end_button);
+        }
     }
 }
