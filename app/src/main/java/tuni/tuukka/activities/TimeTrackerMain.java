1a2e7ac22bdf3e4aa83c087d57e8ae3f698fa13c
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-04 14:06:28 +0300

    TimeTrackerMain: Modify notification channel
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/TimeTrackerMain.java
+++ app/src/main/java/tuni/tuukka/activities/TimeTrackerMain.java
@@ -1,6 +1,9 @@
 package tuni.tuukka.activities;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,7 +29,23 @@ public class TimeTrackerMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v("Author", "Author: Tuukka Juusela - Email: <tuukka.juusela@tuni.fi>");
+        createNotificationChannel();
         setContentView(R.layout.activity_time_tracker_main);
         startActivity(new Intent(this, Authorization.class));
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "time-tracker";
+            String description = "Used to inform user about elapsed work time";
+            NotificationChannel channel = new NotificationChannel("time-tracker", name, NotificationManager.IMPORTANCE_LOW);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
