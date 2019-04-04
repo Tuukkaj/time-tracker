b2587a244938aa3e27e3d8d2995b28f9f9ec923b
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-04 14:07:20 +0300

    TimerService: Send broadcast of timer advancing
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/services/TimerService.java
+++ app/src/main/java/tuni/tuukka/services/TimerService.java
@@ -1,15 +1,31 @@
 package tuni.tuukka.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.format.DateUtils;
+import android.widget.RemoteViews;
 
+import tuni.tuukka.R;
+import tuni.tuukka.activities.Timer;
 
 
 public class TimerService extends Service {
     public final static String TIMER_EVENT_NAME = "time";
-    boolean run;
+    public final static String TIMER_START_PARAM = "start";
+    public final static String TIMER_SEND_PARAM = "seconds";
+    public final static int SERVICE_ID = 14123;
+
+    private boolean run;
+
+    NotificationCompat.Builder builder;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -23,6 +39,7 @@ public class TimerService extends Service {
         new Thread(() -> {
             Intent broadcast = new Intent(TIMER_EVENT_NAME);
             LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
+            int seconds = intent.getIntExtra(TIMER_START_PARAM, 0);
 
             while (run) {
                 try {
@@ -30,8 +47,13 @@ public class TimerService extends Service {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                manager.sendBroadcast(broadcast);
+                if(run) {
+                    seconds++;
+                    broadcast.putExtra(TIMER_SEND_PARAM, seconds);
+                    manager.sendBroadcast(broadcast);
+                    builder.setContentText(DateUtils.formatElapsedTime(seconds));
+                    NotificationManagerCompat.from(this).notify(SERVICE_ID, builder.build());
+                }
             }
         }).start();
 
