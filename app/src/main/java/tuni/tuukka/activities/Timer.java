c2f15849581b324c9bb905106554ba012ed40c41
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-06 20:21:12 +0300

    Timer: Update timer time after clicking start
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/Timer.java
+++ app/src/main/java/tuni/tuukka/activities/Timer.java
@@ -2,7 +2,9 @@ package tuni.tuukka.activities;
 
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +24,8 @@ public class Timer extends AppCompatActivity {
     String startTime;
     String name;
     String id;
+
+    AsyncTask<Void,Void,Void> timeTask;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,17 +48,48 @@ public class Timer extends AppCompatActivity {
     public void onClick(View v) {
         Button startButton = (Button) findViewById(R.id.start_button);
         Button endButton = (Button) findViewById(R.id.end_button);
-        Intent serviceIntent = new Intent(this, TimerService.class);
 
         if(v.getId() == R.id.start_button) {
             startButton.setEnabled(false);
             endButton.setEnabled(true);
             ((TextView) findViewById(R.id.start_text)).setText(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) +":"+Calendar.getInstance().get(Calendar.MINUTE));
             start = System.currentTimeMillis() / 1000;
+            startTime();
         } else if (v.getId() == R.id.end_button){
             startButton.setEnabled(true);
             endButton.setEnabled(false);
+            stopTime();
             ((TextView) findViewById(R.id.timer_time_text)).setText(DateUtils.formatElapsedTime( (System.currentTimeMillis() /1000) - start));
         }
+    }
+
+    public void startTime() {
+        TextView timeText = (TextView) findViewById(R.id.timer_time_text);
+        Context context = this;
+        timeTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                new Thread(() -> {
+                    while(true) {
+                        Timer.this.runOnUiThread(() -> {
+                            timeText.setText(DateUtils.formatElapsedTime((System.currentTimeMillis() / 1000) - start));
+                        });
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                return null;
+            }
+        };
+
+        timeTask.execute();
+    }
+
+    public void stopTime() {
+        timeTask.cancel(true);
     }
 }
