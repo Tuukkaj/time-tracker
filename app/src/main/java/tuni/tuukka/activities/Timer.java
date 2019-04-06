0eea52c251654cc0edbbbc993fa56bd7076116cf
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-06 20:05:13 +0300

    Timer: Use timestamp to record when time was started
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/Timer.java
+++ app/src/main/java/tuni/tuukka/activities/Timer.java
@@ -18,7 +18,8 @@ import tuni.tuukka.R;
 import tuni.tuukka.services.TimerService;
 
 public class Timer extends AppCompatActivity {
-    int seconds = 0;
+    long start;
+    String startTime;
     String name;
     String id;
 
@@ -41,22 +42,19 @@ public class Timer extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        Button start = (Button) findViewById(R.id.start_button);
-        Button end = (Button) findViewById(R.id.end_button);
+        Button startButton = (Button) findViewById(R.id.start_button);
+        Button endButton = (Button) findViewById(R.id.end_button);
         Intent serviceIntent = new Intent(this, TimerService.class);
 
         if(v.getId() == R.id.start_button) {
-            start.setEnabled(false);
-            end.setEnabled(true);
-            serviceIntent.putExtra(TimerService.TIMER_START_PARAM, seconds);
-            serviceIntent.putExtra("sheetName", name);
-            serviceIntent.putExtra("sheetId", id);
-
-            startService(serviceIntent);
+            startButton.setEnabled(false);
+            endButton.setEnabled(true);
+            ((TextView) findViewById(R.id.start_text)).setText(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) +":"+Calendar.getInstance().get(Calendar.MINUTE));
+            start = System.currentTimeMillis() / 1000;
         } else if (v.getId() == R.id.end_button){
-            start.setEnabled(true);
-            end.setEnabled(false);
-            stopService(serviceIntent);
+            startButton.setEnabled(true);
+            endButton.setEnabled(false);
+            ((TextView) findViewById(R.id.timer_time_text)).setText(DateUtils.formatElapsedTime( (System.currentTimeMillis() /1000) - start));
         }
     }
 }
