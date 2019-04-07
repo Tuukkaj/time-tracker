354823f18d8532651e8bd4b65bca3fc22fc5ef7a
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-07 15:29:39 +0300

    ManualTimeInput: Add functionality to upload users data

--- app/src/main/java/tuni/tuukka/activities/ManualTimeInput.java
+++ app/src/main/java/tuni/tuukka/activities/ManualTimeInput.java
@@ -1,12 +1,20 @@
 package tuni.tuukka.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 
 import tuni.tuukka.R;
+import tuni.tuukka.google.DataTime;
+import tuni.tuukka.google.DoAfter;
+import tuni.tuukka.google.SheetApi;
+import tuni.tuukka.google.SheetRequestsInfo;
 
 public class ManualTimeInput extends AppCompatActivity {
     private String name;
@@ -21,13 +29,38 @@ public class ManualTimeInput extends AppCompatActivity {
     }
 
     public void addClicked(View v) {
-        int hours = Integer.parseInt(((EditText) findViewById(R.id.manualtimeinput_hours)).getText().toString());
-        int minutes = Integer.parseInt(((EditText) findViewById(R.id.manualtimeinput_minutes)).getText().toString());
+        String hoursText = ((EditText) findViewById(R.id.manualtimeinput_hours)).getText().toString();
+        String minutesText =((EditText) findViewById(R.id.manualtimeinput_minutes)).getText().toString();
         String comment = ((EditText) findViewById(R.id.manualtimeinput_comment)).getText().toString();
         String category = ((EditText) findViewById(R.id.manualtimeinput_category)).getText().toString();
-        System.out.println(hours);
-        System.out.println(minutes);
-        System.out.println(comment);
-        System.out.println(category);
+
+        if(hoursText.length() > 0 && minutesText.length() > 0) {
+            float hours = Float.parseFloat(hoursText);
+            int minutes = Integer.parseInt(minutesText);
+            float decimalMinutes = minutes / 60f;
+            if(decimalMinutes < 1) {
+                DataTime dataTime = new DataTime(Math.round((hours+decimalMinutes) * 100f) / 100f, comment, category, new SheetRequestsInfo(id, "work"));
+                SheetApi.appendSheet(dataTime, createInterface());
+            }
+        } else {
+            Toast.makeText(this, "Enter time please", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private DoAfter<AppendValuesResponse> createInterface() {
+        return new DoAfter<AppendValuesResponse>() {
+            @Override
+            public void onSuccess(AppendValuesResponse value) {
+                ManualTimeInput.this.startActivity(new Intent(ManualTimeInput.this, Authorization.class));
+                ManualTimeInput.this.runOnUiThread(()  -> {
+                    Toast.makeText(ManualTimeInput.this, "Data uploaded", Toast.LENGTH_SHORT).show();
+                });
+            }
+
+            @Override
+            public void onFail() {
+                Toast.makeText(ManualTimeInput.this, "Check your connection status", Toast.LENGTH_SHORT).show();
+            }
+        };
     }
 }
