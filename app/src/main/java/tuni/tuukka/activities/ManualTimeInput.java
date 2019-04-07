e7438e32baa56d9a55bb75e08f9e04d0cd22dfaa
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-07 22:26:06 +0300

    ManualTimeInput: Add sheets name to top of application

--- app/src/main/java/tuni/tuukka/activities/ManualTimeInput.java
+++ app/src/main/java/tuni/tuukka/activities/ManualTimeInput.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.api.services.sheets.v4.model.AppendValuesResponse;
@@ -17,15 +18,17 @@ import tuni.tuukka.google.SheetApi;
 import tuni.tuukka.google.SheetRequestsInfo;
 
 public class ManualTimeInput extends AppCompatActivity {
-    private String name;
     private String id;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manualtimeinput);
-        name = getIntent().getStringExtra("sheetName");
+        String name = getIntent().getStringExtra("sheetName");
         id = getIntent().getStringExtra("sheetId");
+
+        ((TextView) findViewById(R.id.manualtimeinput_sheetName)).setText(name.substring(13));
+        ((TextView) findViewById(R.id.manualtimeinput_sheetId)).setText(id);
     }
 
     public void addClicked(View v) {
