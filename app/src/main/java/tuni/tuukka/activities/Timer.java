65537d33dbc0aa29661fdd9114bae3a1b87bf7ee
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-07 11:03:50 +0300

    Timer: Save tracked time to preferences correctly
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- app/src/main/java/tuni/tuukka/activities/Timer.java
+++ app/src/main/java/tuni/tuukka/activities/Timer.java
@@ -2,16 +2,15 @@ package tuni.tuukka.activities;
 
 
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateUtils;
 import android.view.View;
@@ -19,10 +18,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-import org.w3c.dom.Text;
-
 import java.util.Calendar;
-import java.util.Date;
+
 
 import tuni.tuukka.R;
 
@@ -31,7 +28,7 @@ public class Timer extends AppCompatActivity {
     String name;
     String id;
     boolean runTimer;
-
+    boolean saveTime = true;
     AsyncTask<Void,Void,Void> timeTask;
 
     @Override
@@ -62,15 +59,21 @@ public class Timer extends AppCompatActivity {
             ((TextView) findViewById(R.id.start_text)).setText(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) +":"+Calendar.getInstance().get(Calendar.MINUTE));
             start = System.currentTimeMillis() / 1000;
             startTime();
+            createNotification();
         } else if (v.getId() == R.id.end_button){
             startButton.setEnabled(true);
             endButton.setEnabled(false);
             stopTime();
-            ((TextView) findViewById(R.id.timer_time_text)).setText(DateUtils.formatElapsedTime( (System.currentTimeMillis() /1000) - start));
+
+            getPreferences(Context.MODE_PRIVATE).edit().clear().commit();
+            saveTime = false;
+            System.out.println(getSharedPreferences("time-tracker", Context.MODE_PRIVATE).getLong("start",123));
+            System.out.println(getSharedPreferences("time-tracker", Context.MODE_PRIVATE).getString("time-text","TYHJÄ"));
+
             Intent nextActivity = new Intent(this, Upload.class);
             nextActivity.putExtra("sheetName", name);
             nextActivity.putExtra("sheetId", id);
-            float hoursWorked = ((System.currentTimeMillis() / 1000) - start)/ 3600f;
+            float hoursWorked = ((System.currentTimeMillis() / 1000) - start);
             nextActivity.putExtra("time", hoursWorked);
             startActivity(nextActivity);
         }
@@ -101,11 +104,15 @@ public class Timer extends AppCompatActivity {
         };
 
         timeTask.execute();
+    }
 
+    public void createNotification() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "time-tracker")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Started tracking work time")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Notification notification = builder.build();
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
         getSystemService(NotificationManager.class).notify(123, builder.build());
     }
 
@@ -117,11 +124,16 @@ public class Timer extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        System.out.println("PAUSE" + start);
-        String text = ((TextView) findViewById(R.id.start_text)).getText().toString();
-        getSharedPreferences("time-tracker", Context.MODE_PRIVATE).edit().putLong("start",start).apply();
-        getSharedPreferences("time-tracker", Context.MODE_PRIVATE).edit().putString("time-text",text).apply();
+        SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE).edit();
 
+        if(saveTime) {
+            String text = ((TextView) findViewById(R.id.start_text)).getText().toString();
+            editor.putLong("start", start);
+            editor.putString("time-text", text);
+            editor.commit();
+        } else {
+            editor.clear().commit();
+        }
         stopTime();
     }
 
@@ -129,18 +141,21 @@ public class Timer extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         long time = System.currentTimeMillis() / 1000;
-        start = getSharedPreferences("time-tracker", Context.MODE_PRIVATE).getLong("start",time);
-        String text = getSharedPreferences("time-tracker", Context.MODE_PRIVATE).getString("time-text","");
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
 
-        if(time != start) {
+        start = preferences.getLong("start",time);
+        String text = preferences.getString("time-text","");
+
+        System.out.println(time);
+        System.out.println(start);
+
+        if(time != start && start != 1) {
             startTime();
             ((TextView) findViewById(R.id.start_text)).setText(text);
+            ((Button) findViewById(R.id.start_button)).setEnabled(false);
+            ((Button) findViewById(R.id.end_button)).setEnabled(true);
         }
 
-        getSharedPreferences("time-tracker", Context.MODE_PRIVATE).edit().remove("start").apply();
-        getSharedPreferences("time-tracker", Context.MODE_PRIVATE).edit().remove("time-text").apply();
-
-
-        System.out.println("RESUME" + start);
+        preferences.edit().clear().commit();
     }
 }
