1ecb3e3ab12beed9f786f715bd1b95f7abe90275
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-18 23:15:44 +0300

    LoadingScreenHelper: Add color change animation to rotating icon

--- app/src/main/java/tuni/tuukka/activity_helper/LoadingScreenHelper.java
+++ app/src/main/java/tuni/tuukka/activity_helper/LoadingScreenHelper.java
@@ -1,6 +1,9 @@
 package tuni.tuukka.activity_helper;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
@@ -9,6 +12,24 @@ import tuni.tuukka.R;
 public class LoadingScreenHelper {
     public static void start(Activity activity)  {
         activity.setContentView(R.layout.loading_screen);
-        ((ImageView) activity.findViewById(R.id.loading)).setAnimation(AnimationUtils.loadAnimation(activity, R.anim.rotation));
+        ImageView view = (ImageView) activity.findViewById(R.id.loading);
+        view.setAnimation(AnimationUtils.loadAnimation(activity, R.anim.rotation));
+
+        int colorFrom = activity.getResources().getColor(R.color.colorPrimary);
+        int colorTo = activity.getResources().getColor(R.color.colorAccent);
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+        colorAnimation.setDuration(1000); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                DrawableCompat.setTint(view.getDrawable(), (int)animator.getAnimatedValue());
+
+            }
+
+        });
+        colorAnimation.setRepeatMode(ValueAnimator.REVERSE);
+        colorAnimation.setRepeatCount(ValueAnimator.INFINITE);
+        colorAnimation.start();
+
     }
 }
