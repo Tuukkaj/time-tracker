c6897b6cdb35d1ee1d242a5f795a4a34e401964d
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-21 11:45:04 +0300

    About: Set title to "About"

--- app/src/main/java/tuni/tuukka/activities/About.java
+++ app/src/main/java/tuni/tuukka/activities/About.java
@@ -11,7 +11,7 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        getSupportActionBar().setTitle("About");
     }
 
 }
