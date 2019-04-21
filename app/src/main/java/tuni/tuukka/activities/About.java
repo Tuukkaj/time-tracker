198fda22cb320160c2851aa3c142e0622b556577
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-21 11:22:45 +0300

    About: Create About activity

---
+++ app/src/main/java/tuni/tuukka/activities/About.java
@@ -0,0 +1,17 @@
+package tuni.tuukka.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tuni.tuukka.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+
+}
