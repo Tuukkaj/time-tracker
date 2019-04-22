913a1d504125c6f0448952dc2ae0699f214774df
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-22 21:26:39 +0300

    AccountAuthorization: Add exception handling if something goes wrong

--- app/src/main/java/tuni/tuukka/google/AccountAuthorization.java
+++ app/src/main/java/tuni/tuukka/google/AccountAuthorization.java
@@ -4,10 +4,13 @@ import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
+import android.widget.Toast;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.services.drive.DriveScopes;
+
+import tuni.tuukka.R;
+
 /**
  * @author      Tuukka Juusela <tuukka.juusela@tuni.fi>
  * @version     20190323
@@ -26,12 +29,16 @@ public class AccountAuthorization {
         activity.runOnUiThread(() -> {
             AccountManager am = AccountManager.get(activity);
             Bundle options = new Bundle();
-            am.getAuthToken(credential.getSelectedAccount(), "oauth2:" + DriveScopes.DRIVE,
-                    options, activity, new TokenAcquired(activity),
-                    new Handler(message -> {
-                        System.out.println(message);
-                        return true;
-                    }));
+            try {
+                am.getAuthToken(credential.getSelectedAccount(), "oauth2:" + DriveScopes.DRIVE,
+                        options, activity, new TokenAcquired(activity),
+                        new Handler(message -> {
+                            System.out.println(message);
+                            return true;
+                        }));
+            } catch (Exception e) {
+                activity.runOnUiThread(() -> Toast.makeText(activity, activity.getString(R.string.error_general), Toast.LENGTH_SHORT).show());
+            }
         });
     }
 }
